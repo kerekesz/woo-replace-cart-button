24cadf50c7dfde2b81cdd899139dd2d9d39c98e8
Zoltán Kerekes <kezoo.webdesign@gmail.com>
2019-04-01 15:30:56 +0200

    Update woo-replace-cart-button.php

--- woo-replace-cart-button.php
+++ woo-replace-cart-button.php
@@ -1,5 +1,3 @@
-// Replace add to cart button by a linked button to the product in Shop and archives pages
-
 add_filter( 'woocommerce_loop_add_to_cart_link', 'replace_loop_add_to_cart_button', 10, 2 );
 function replace_loop_add_to_cart_button( $button, $product  ) {
 
